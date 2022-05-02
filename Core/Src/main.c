d060aa7253e2921fbcf91d06e0383435a891efc1
Gantulga G <limited.tulgaa@gmail.com>
2022-05-02 12:36:48 +0800

    stash after MX update

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -603,9 +603,9 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_Init(DDC1_SYNC_NCO_GPIO_Port, &GPIO_InitStruct);
 
   /* EXTI interrupt init*/
-  HAL_NVIC_SetPriority(EXTI0_IRQn, 0, 0);
+  HAL_NVIC_SetPriority(EXTI0_IRQn, 1, 0);
 
-  HAL_NVIC_SetPriority(EXTI9_5_IRQn, 0, 0);
+  HAL_NVIC_SetPriority(EXTI9_5_IRQn, 2, 0);
   HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
 
 }
